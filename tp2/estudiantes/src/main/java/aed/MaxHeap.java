04d031e95eff4bbdb1ad9109c4d9e46e842951cd
Joaquín Campos Haedo <joaquincamposhaedo@gmail.com>
2025-11-09 22:16:38 -0300

    MaxHeap probablemente completo. Falta definir Handle

--- tp2/estudiantes/src/main/java/aed/MaxHeap.java
+++ tp2/estudiantes/src/main/java/aed/MaxHeap.java
@@ -15,21 +15,37 @@ public class MaxHeap<T extends Comparable> {
             valor = v;
             posicion = pos;
         }
-
-        public Nodo padre() {
-            return _nodos.get((posicion -1) / 2);
-        }
         
-        public int posicion() {
-            return posicion;
+        public boolean tienePadre() {
+            return esPosicionValida((posicion-1) / 2);
         }
 
         public boolean tieneHijoIzq() {
-            return ;
+            return esPosicionValida(posicion * 2);
         }
-        
-        public boolean tieneHijoDer() {
 
+        public boolean tieneHijoDer() {
+            return esPosicionValida(posicion * 2 + 1);
+        }
+
+        public Nodo padre() {
+            return _nodos.get((posicion-1) / 2);
+        }
+
+        public Nodo izq() {
+            return _nodos.get(posicion * 2);
+        }
+
+        public Nodo der() {
+            return _nodos.get(posicion * 2 + 1);
+        }
+
+        public Nodo hijoMayor() {   // asume que hay por lo menos un hijo izq (al ser izquierdista, no puede haber hijo der y no izq)
+            Nodo res = izq();
+            if (tieneHijoDer() && (der().valor).compareTo(izq().valor) > 0) { // si tiene hijo der, es mayor que el izq?
+                res = der(); 
+            }
+            return res;
         }
     }
     
@@ -57,52 +73,36 @@ public class MaxHeap<T extends Comparable> {
 
     // metodos aux
 
-    private void siftUpNodo(Nodo n) {
-        
-        while (n.padre().posicion >= 0 && n.padre().valor.compareTo(n.valor) > 0) {       // mientras el padre es mayor al nodo actual
-            swap(n.padre().posicion, n.posicion);
-        }
-    }
-
-    private void siftDownNodo(Nodo n) {
-        while (n.tieneAlgunHijo()) {
-            
-        }
-    }
-
+    
     private boolean esPosicionValida(int i) {
         return (i >= 0 && i < _nodos.size());
     }
-
-    private boolean tieneAlgunHijo(Nodo n) {
-        return i < _nodos.size()/2 -1;
-
+    
+    private void swapPos(Nodo a, Nodo b) {
         
+        int posA = a.posicion;
+        int posB = b.posicion;
+
+        _nodos.set(posB, a);
+        _nodos.set(posA, b);
+
+        a.posicion = posB;
+        b.posicion = posA;
     }
 
-    private void siftDownNodo(Nodo n){
-        while(n.tiene()){
-            swap(n.posicion, hijoMayor(n).posicion);
+    private void siftUp(Nodo n) {
+        
+        while (n.tienePadre() && (n.padre().valor).compareTo(n.valor) > 0) {       // mientras el padre es mayor al nodo actual
+            swapPos(n.padre(), n);
         }
     }
 
-    private boolean algunHijoEsMayorANodo(Nodo n){
-        Nodo hijoMenor = _nodos.get(2*n.posicion+1);
-        Nodo hijoMayor = _nodos.get(2*n.posicion+2);
-        return n.valor.compareTo(hijoMayor.valor) < 0 || n.valor.compareTo(hijoMenor.valor) < 0;
-    };
-
-    private Nodo hijoMayor(n){
-        Nodo hijoMenor = _nodos.get(2*n.posicion+1);
-        Nodo hijoMayor = _nodos.get(2*n.posicion+2);
-        if ()
-    };
-
-    private void swap(int i, int j) {
-        Nodo nodoI = _nodos.get(i);
-        Nodo nodoJ = _nodos.get(i);
-        _nodos.set(i, nodoJ);
-        _nodos.set(j, nodoI);
+    private void siftDown(Nodo n) {
+        
+        // tiene hijo izq o tiene hijo der, y alguno es mayor
+        while (n.tieneHijoIzq() && (n.hijoMayor().valor).compareTo(n.valor) > 0) {    // mientras haya un hijo mayor
+            swapPos(n, n.hijoMayor());
+        }
     }
 
     // fin metodos aux
@@ -112,16 +112,31 @@ public class MaxHeap<T extends Comparable> {
         _nodos = new ArrayList<Nodo>();
     }
 
+    public boolean estaVacio() {
+        return _nodos.size() == 0;
+    }
+
+    public T consultarMaximo() {
+        return _nodos.get(0).valor;
+    }
+
+    public void encolar(T valor) {
+        Nodo nuevoNodo = new Nodo(valor, _nodos.size());
+        _nodos.addLast(nuevoNodo);
+        siftUp(nuevoNodo);
+    }
+
     public T desencolar() {
-        // remover _nodos[0]
-        throw new UnsupportedOperationException("Sin implementar");
-    }
+        
+        Nodo aDesencolar = _nodos.get(0);
+        Nodo reemplazoDeMaximo = _nodos.get(_nodos.size()-1);
+        
+        swapPos(reemplazoDeMaximo, aDesencolar);
 
-    public void siftUp() {
-        throw new UnsupportedOperationException("Sin implementar");
-    }
+        T valorDeDesencolado = _nodos.remove(aDesencolar.posicion).valor;
 
-    public void siftDown() {
-        throw new UnsupportedOperationException("Sin implementar");
-    }   
+        siftDown(reemplazoDeMaximo);
+        
+        return valorDeDesencolado;
+    }
 }
